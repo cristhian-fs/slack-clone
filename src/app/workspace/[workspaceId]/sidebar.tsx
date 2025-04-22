f9bf3ed8f1dc802d337d0a362cff13469fbbca31
Cristhian <cristhianfernandolp@gmail.com>
2025-04-22 11:36:33 -0300

    chore: remove isActive hardcoded property

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -7,7 +7,7 @@ export const Sidebar = () => {
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-9 pb-4">
       <WorkspaceSwitcher />
-      <SidebarButton icon={Home} label="Home" isActive />
+      <SidebarButton icon={Home} label="Home" />
       <SidebarButton icon={MessagesSquareIcon} label="DMs" />
       <SidebarButton icon={BellIcon} label="Activity" />
       <SidebarButton icon={MoreHorizontal} label="More" />
